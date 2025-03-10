8257522f99b9f66949b7425291a2c5eb7a6b0904
harunashi75 <harunashi75@gmail.com>
2025-03-10 14:42:10 -0400

    Mise à jour v0.4.0

--- Assets/Scenes/pause_menu.gd
+++ Assets/Scenes/pause_menu.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var resume_button = $ResumeButton  # Chemin correct vers le bouton Resume
 @onready var quit_button = $QuitButton  # Chemin correct vers le bouton Quit
 @onready var main_menu_button = $MainMenuButton  # Chemin correct vers le bouton Main Menu
+@onready var volume_slider = $VolumeSlider
 
 func _ready():
 	print("PauseMenu visible:", pause_menu.visible)
@@ -11,8 +12,29 @@ func _ready():
 	quit_button.pressed.connect(_on_quit_button_pressed)
 	main_menu_button.pressed.connect(_on_main_menu_button_pressed)
 
+	# Charger le volume sauvegardé
+	var saved_volume = load_volume()
+	volume_slider.value = saved_volume
+	_on_volume_changed(saved_volume)
+	volume_slider.value_changed.connect(_on_volume_changed)
+
 	pause_menu.hide()  # Cache le menu pause au début
 	set_process(true)  # Active le process pour surveiller ESC
+
+func _on_volume_changed(value):
+	AudioServer.set_bus_volume_db(0, linear_to_db(value))
+	save_volume(value)
+
+func save_volume(value):
+	var config = ConfigFile.new()
+	config.set_value("audio", "volume", value)
+	config.save("user://settings.cfg")
+
+func load_volume():
+	var config = ConfigFile.new()
+	if config.load("user://settings.cfg") == OK:
+		return config.get_value("audio", "volume", 1.0)
+	return 1.0  # Volume par défaut
 
 func _process(_delta):
 	var viewport_size = get_viewport_rect().size
--- Assets/Scripts/main_menu.gd
+++ Assets/Scripts/main_menu.gd
@@ -2,33 +2,11 @@ extends Control
 
 @onready var play_button = $PlayButton
 @onready var exit_button = $ExitButton
-@onready var volume_slider = $VolumeSlider
 
 func _ready():
 	play_button.pressed.connect(_on_play_pressed)
 	exit_button.pressed.connect(_on_quit_pressed)
 	
-	# Charger le volume sauvegardé
-	var saved_volume = load_volume()
-	volume_slider.value = saved_volume
-	_on_volume_changed(saved_volume)
-	volume_slider.value_changed.connect(_on_volume_changed)
-
-func _on_volume_changed(value):
-	AudioServer.set_bus_volume_db(0, linear_to_db(value))
-	save_volume(value)
-
-func save_volume(value):
-	var config = ConfigFile.new()
-	config.set_value("audio", "volume", value)
-	config.save("user://settings.cfg")
-
-func load_volume():
-	var config = ConfigFile.new()
-	if config.load("user://settings.cfg") == OK:
-		return config.get_value("audio", "volume", 1.0)
-	return 1.0  # Volume par défaut
-
 func _process(_delta):
 	if Input.is_action_just_pressed("play"):
 		_on_play_pressed()
