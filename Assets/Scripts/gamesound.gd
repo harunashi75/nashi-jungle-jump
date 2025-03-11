47ae95482da46d6b8c7149eeda296f6675e930c1
harunashi75 <harunashi75@gmail.com>
2025-03-11 12:08:16 -0400

    Mise à jour v0.4.1

--- Assets/Scripts/gamesound.gd
+++ Assets/Scripts/gamesound.gd
@@ -2,3 +2,8 @@ extends AudioStreamPlayer
 
 func _ready():
 	play()  # Joue la musique au démarrage
+
+func _exit_tree():
+	if $"/root/Main/Audio/GameSound":
+		$"/root/Main/Audio/GameSound".stop()
+		$"/root/Main/Audio/GameSound".stream = null  # Libère la musique de la mémoire
